0d9744d03ff97a8f7a84835760ec4336f3d596df
Notebook <muthurimutuma@yahoo.com>
2013-12-05 21:52:49 +0300

    Initialized app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dbhiv::Application.config.secret_key_base = '692f8d6b14dc1d9c2f1ec4877ed68114ed65d8a7d7282fc39c8b7c3f263f14d43f4e38a254cfbb1cc52a30875b51dee51cb1dcc6e3fd9ee5a96ac1545992ec79'
